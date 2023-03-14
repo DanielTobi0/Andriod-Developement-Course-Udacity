5cd4702c9829596ba26caf312f2bdeb234609ef7
DanielTobi0 <Adejumobidaniel563@gmail.com>
2023-03-14 12:14:42 +0100

    replace findViewById func to Binding

--- AboutMe-app/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe-app/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -7,31 +7,34 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.doneButton.setOnClickListener {
             addNickName(it)
         }
-
-
     }
 
     private fun addNickName(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edt)
-        val nickNameTextView = findViewById<EditText>(R.id.nickname_text)
+        binding.apply {
+            binding.nicknameText.text = binding.nicknameEdit.text
+            binding.nicknameEdit.visibility = View.GONE
+            binding.doneButton.visibility = View.GONE
+            binding.nicknameText.visibility = View.VISIBLE
 
+        }   
 
-        nickNameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        editText.visibility = View.VISIBLE
 
         //To hide keyboard after entering text
-        val hideKeyboard = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val hideKeyboard = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         hideKeyboard.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
