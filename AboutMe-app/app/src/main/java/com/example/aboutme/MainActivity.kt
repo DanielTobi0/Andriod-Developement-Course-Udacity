8c15e628edbfab30f3828d8ef30cd143c393aa5a
DanielTobi0 <Adejumobidaniel563@gmail.com>
2023-03-14 15:50:42 +0100

    imports and line clean ups

--- AboutMe-app/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe-app/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.aboutme
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
 import androidx.databinding.DataBindingUtil
 import com.example.aboutme.databinding.ActivityMainBinding
 
@@ -14,10 +11,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val myName: MyName = MyName("Daniel Tobi")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
         binding.doneButton.setOnClickListener {
             addNickName(it)
         }
@@ -25,12 +25,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun addNickName(view: View) {
         binding.apply {
-            binding.nicknameText.text = binding.nicknameEdit.text
+
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
             binding.nicknameEdit.visibility = View.GONE
             binding.doneButton.visibility = View.GONE
             binding.nicknameText.visibility = View.VISIBLE
 
-        }   
+        }
 
 
         //To hide keyboard after entering text
