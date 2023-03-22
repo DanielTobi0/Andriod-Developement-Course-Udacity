bedce5c8e28a9b8e4c9dc5adaff921bd7eb0a539
DanielTobi0 <adejumobidaniel563@gmail.com>
2023-03-22 09:23:12 +0100

    added -> end game event

--- word_trivia-app/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ word_trivia-app/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -41,8 +41,12 @@ class GameViewModel : ViewModel() {
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish: LiveData<Boolean>
+        get() = _eventGameFinish
 
     init {
+        _eventGameFinish.value = false
         resetList()
         nextWord()
         _score.value = 0
@@ -84,7 +88,8 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         //Select and remove a word from the list
         if (wordList.isEmpty()) {
-            // gameFinished() should happen here
+            _eventGameFinish.value = true
+
         } else {
             _word.value = wordList.removeAt(0)
         }
@@ -100,5 +105,10 @@ class GameViewModel : ViewModel() {
     fun onCorrect() {
         _score.value = (score.value)?.plus(1)
         nextWord()
+    }
+
+
+    fun onGameFinishComplete(){
+        _eventGameFinish.value = false
     }
 }
